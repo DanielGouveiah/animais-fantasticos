70e981db11fb7d8687af5442ae667db2cc4188a3
danielgouveiah <nel_hue@hotmail.com>
2022-09-21 20:07:25 -0300

    dropdown menu refatorado

--- js/modules/dropdownMenu.js
+++ js/modules/dropdownMenu.js
@@ -1,17 +1,36 @@
 import outSideClick from "./outSideClick.js";
 
-export default function initDropDown() {
-  const menus = document.querySelectorAll("[data-dropdown]");
-  function toggleDropdown(e) {
+export default class DropdownMenu {
+  constructor(dropdownMenus, events) {
+    this.menus = document.querySelectorAll(dropdownMenus);
+    if (events === undefined) this.events = ["click", "touchstart"];
+    else this.events = events;
+    this.activeClass = "ativo";
+
+    this.activeDropdown = this.activeDropdown.bind(this);
+  }
+
+  activeDropdown(e) {
     e.preventDefault();
-    this.classList.add("ativo");
-    outSideClick(this, ["click", "touchstart"], () => {
-      this.classList.remove("ativo");
+    const element = e.currentTarget;
+    element.classList.add(this.activeClass);
+    outSideClick(element, this.events, () => {
+      element.classList.remove(this.activeClass);
     });
   }
-  menus.forEach((item) => {
-    ["click", "touchstart"].forEach((userEvent) => {
-      item.addEventListener(userEvent, toggleDropdown);
+
+  addDropdownMenuEvent() {
+    this.menus.forEach((item) => {
+      ["click", "touchstart"].forEach((userEvent) => {
+        item.addEventListener(userEvent, this.activeDropdown);
+      });
     });
-  });
+  }
+
+  init() {
+    if (this.menus.length) {
+      this.addDropdownMenuEvent();
+    }
+    return this;
+  }
 }
